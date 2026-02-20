cd2ac71c831351f716371bd4987427d2348db876
enzo <flexibank@gmail.com>
2026-02-20 12:11:06 +0100

    Update Ticket controller

--- src/Controller/Ticket.php
+++ src/Controller/Ticket.php
@@ -2,10 +2,31 @@
 
 namespace App\Controllers;
 
+use App\Core\Database;
+use App\Repository\TicketRepository;
+
 class Ticket
 {
-    public function __construct()
+    // get tous les tickets
+    public static function all()
     {
+        $pdo = Database::getInstance()->pdo;
+        $AllTickets = (new TicketRepository($pdo))->getAllTickets();
 
+        $html = '
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Tickets</title>
+        </head>
+        <body>
+            ' . var_dump($AllTickets) . '
+        </body>
+        </html>
+        ';
+
+        return $html;
     }
 }
